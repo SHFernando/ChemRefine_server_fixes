00b5ba41bac2a1b3ac471e89b012c2e0038d92d1
Ignacio Migliaro <ignaciomigliaro@outlook.com>
2026-02-18 12:10:56 -0600

    Refactor and organize public API and data structures in ChemRefine

--- src/chemrefine/__init__.py
+++ src/chemrefine/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 ChemRefine: Automated conformer sampling and refinement using ORCA.
 
@@ -10,6 +9,39 @@ __version__ = "1.3.1"
 __author__ = "Sterling Group"
 __email__ = "dal950773@utdallas.edu"
 
+
+# ------------------------
+# Public data structures
+# ------------------------
+
+from .types import (
+    EngineConfig,
+    SamplingConfig,
+    StepContext,
+    StepResult,
+    PipelineState,
+)
+
+
+# ------------------------
+# Public main interface
+# ------------------------
+
 from .core import ChemRefiner
 
-__all__ = ["ChemRefiner", "run_mlff_calculation", "MLFFJobSubmitter"]
+
+# ------------------------
+# Public API
+# ------------------------
+
+__all__ = [
+    # Main class
+    "ChemRefiner",
+
+    # Pipeline types
+    "EngineConfig",
+    "SamplingConfig",
+    "StepContext",
+    "StepResult",
+    "PipelineState",
+]
